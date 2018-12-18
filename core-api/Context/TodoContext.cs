d15291924d238b0f163c2c287f05df85a42f0104
dfcook <dfcook@hotmail.com>
2018-12-18 18:47:18 +0000

    persistent volumes and migrations

--- core-api/Context/TodoContext.cs
+++ core-api/Context/TodoContext.cs
@@ -10,7 +10,6 @@ namespace core_api.Context
     public TodoContext(DbContextOptions<TodoContext> options)
         : base(options)
     {
-      this.Database.EnsureCreated();
     }
 
     public virtual DbSet<Todo> Todos { get; set; }
@@ -21,12 +20,12 @@ namespace core_api.Context
       {
         entity.Property(e => e.Id).IsRequired();
         entity.Property(e => e.Text).IsRequired();
+        entity.Property(e => e.UserId).IsRequired().HasDefaultValue("baz");
 
         entity.HasKey(e => e.Id);
 
         entity.HasData(
-          new Todo { Id = 1, Text = "Foo" },
-          new Todo { Id = 2, Text = "Bar" }
+          new Todo { Id = 3, Text = "Baz", UserId = "baz" }
         );
       });
     }
--- core-api/Model/Todo.cs
+++ core-api/Model/Todo.cs
@@ -4,5 +4,6 @@ namespace core_api.Model
   {
     public long Id { get; set; }
     public string Text { get; set; }
+    public string UserId { get; set; }
   }
 }
